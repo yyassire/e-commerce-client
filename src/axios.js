4af757bddb5a4db664c5c59e3d965cfeb61aee34
yyassire02 <yyassire02@gmail.com>
2021-11-28 17:44:09 +0300

    added the r url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 export const publicBaseUrl = axios.create({
-    baseURL: window.location.hostname === "localhost"? "http://localhost:8800/api/" :"https://yy-e-commerce.herokuapp.com/",
+    baseURL: window.location.hostname === "localhost"? "http://localhost:8800/api/" :"https://yy-e-commerce-server.herokuapp.com/api/",
 })
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxN2QyNjU0OTY5MGQwN2Q3OTUwOTgxYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNjMxMzA5M30.soeMIEtD0CHxHdJkFWsiRPpK7XSRvKyxOUMn9Iyer5k"
 // const myStorage = localStorage.getItem("persist:root")
@@ -10,6 +10,6 @@ const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxN2QyNjU0OTY5MGQw
 // console.log("ok")
 
 export const tokenBaseUrl = axios.create({
-    baseURL: window.location.hostname === "localhost"? "http://localhost:8800/api/" : "https://yy-e-commerce.herokuapp.com/",
+    baseURL: window.location.hostname === "localhost"? "http://localhost:8800/api/" : "https://yy-e-commerce-server.herokuapp.com/api/",
     headers: {token : `Bearer ${token}`}
 })
